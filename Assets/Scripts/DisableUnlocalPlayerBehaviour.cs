f0bdf0b6fc1053d3b91714f471482931aa4d9a79
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-05-15 23:54:17 +0900

    Initial Commit

---
+++ Assets/Scripts/DisableUnlocalPlayerBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisableUnlocalPlayerBehaviour : NetworkBehaviour {
+    [SerializeField]
+    private Behaviour[] behaviours;
+
+    public void Start() {
+        if (!isLocalPlayer) {
+            foreach (var behaviour in behaviours) {
+                behaviour.enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+    public Transform Muzzle;
+    public GameObject Bullet;
+    public float Power;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            CmdFire();
+        }
+
+        if (transform.position.y < -25.0f) {
+            transform.position = new Vector2(0.0f, 1.0f);
+        }
+    }
+
+    [Command]
+    void CmdFire() {
+        var bullet = Instantiate(Bullet, Muzzle.position, Quaternion.identity);
+        var rigidbody = bullet.GetComponent<Rigidbody>();
+        rigidbody.velocity = Power * transform.forward;
+        NetworkServer.Spawn(bullet);
+        Destroy(bullet, 5.0f);
+    }
+}
